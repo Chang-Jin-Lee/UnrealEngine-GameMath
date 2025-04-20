2cf653eb34b1584520f207fdddf59da283aea67e
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-04-20 13:45:14 +0900

    [feat] Leibniz Get Pi

---
+++ GameMath/Source/GameMath/LeibnizGetPI.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LeibnizGetPI.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+ULeibnizGetPI::ULeibnizGetPI()
+{
+	UE_LOG(LogTemp, Warning, TEXT("PI value (UKismetMathLibrary::GetPI()) : %f"), UKismetMathLibrary::GetPI());
+	UE_LOG(LogTemp, Warning, TEXT("PI value (LeibnizGetPI) : %f"), CalculatePi(1000));
+}
+
+ULeibnizGetPI::~ULeibnizGetPI()
+{
+}
+
+double ULeibnizGetPI::CalculatePi(int terms)
+{
+	double pi = 0;
+	int sign = 1;
+
+	for (int k = 0; k <= terms; k++) {
+		pi += 4.0 * sign / (2 * k + 1);
+		sign *= -1;
+	}
+	return pi;
+}
---
+++ GameMath/Source/GameMath/LeibnizGetPI.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LeibnizGetPI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEMATH_API ULeibnizGetPI : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	ULeibnizGetPI();
+	~ULeibnizGetPI();
+
+	double CalculatePi(int terms);
+};
