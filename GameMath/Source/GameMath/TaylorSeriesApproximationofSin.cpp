7c5c8193ce4bdb4a73b1667759d3fa76335f3ba0
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-04-20 13:44:37 +0900

    [feat] TaylorSeries Get Approximation of Sin

---
+++ GameMath/Source/GameMath/TaylorSeriesApproximationofSin.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TaylorSeriesApproximationofSin.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+UTaylorSeriesApproximationofSin::UTaylorSeriesApproximationofSin()
+{
+	float xDegrees = 45.0f;
+	float xRadians = xDegrees * UKismetMathLibrary::GetPI() / 180.0f;
+	float result = ApproximateSin(xRadians, maxTermCount);
+
+	UE_LOG(LogTemp, Warning, TEXT("Approximated sin(%f) = (%f)"), xDegrees, result);
+	UE_LOG(LogTemp, Warning, TEXT("FMath::Sin(%f) = (%f)"), xDegrees, xRadians);
+}
+
+UTaylorSeriesApproximationofSin::~UTaylorSeriesApproximationofSin()
+{
+}
+
+float UTaylorSeriesApproximationofSin::ApproximateSin(float x, int MaxTermCount)
+{
+	float result = 0.0f;
+	float numerator = x;			  // x^(2n+1), 처음은 x^1
+	float denominator = 1.0f;         // (2n+1)!
+	int sign = 1;
+
+	for (int n = 0; n < MaxTermCount; n++)
+	{
+		result += sign * (numerator / denominator);
+
+		// 다음 항을 위한 업데이트
+		int power = 2 * n + 3;      // 다음 항의 지수
+		numerator *= x * x;
+		denominator *= (2 * n + 2) * (2 * n + 3);
+		sign *= -1;
+	}
+
+	return result;
+}
---
+++ GameMath/Source/GameMath/TaylorSeriesApproximationofSin.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "TaylorSeriesApproximationofSin.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEMATH_API UTaylorSeriesApproximationofSin : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UTaylorSeriesApproximationofSin();
+	~UTaylorSeriesApproximationofSin();
+
+	float ApproximateSin(float x, int MaxTermCount);
+
+private:
+	int maxTermCount = 10;
+};
