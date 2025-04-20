fe0c1bd49a4f4f2435c104a9c455554a1ad0f023
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-04-20 13:45:53 +0900

    [feat] Actor Bundle to show Math Functions

---
+++ GameMath/Source/GameMath/MathBundle.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MathBundle.h"
+
+#include "LeibnizGetPI.h"
+#include "MontecarloPI.h"
+#include "MontecarloPIActor.h"
+#include "TaylorSeriesApproximationofSin.h"
+
+
+AMathBundle::AMathBundle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMathBundle::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ApproximationofSin = NewObject<UTaylorSeriesApproximationofSin>();
+	LeibnizGetPI = NewObject<ULeibnizGetPI>();
+	MontecarloPI = NewObject<UMontecarloPI>();
+}
+
+void AMathBundle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	CreatedotActor();
+}
+
+void AMathBundle::CreatedotActor()
+{
+	FVector dotPosition = FVector(FMath::RandRange(-500.0f, 500.0f), FMath::RandRange(-500.0f, 500.0f), 50.0f);
+	AActor* SpawnedActor = GetWorld()->SpawnActor(dotActorClass, &dotPosition);
+	if (AMontecarloPIActor* monteActor = Cast<AMontecarloPIActor>(SpawnedActor))
+	{
+		monteActor->MontecarloPI = MontecarloPI;
+	}
+	SpawnedActor->SetLifeSpan(5.0f);
+	MontecarloPI->IncreaseDots();
+	MontecarloPI->ShowValues();
+}
---
+++ GameMath/Source/GameMath/MathBundle.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MathBundle.generated.h"
+
+class UMontecarloPI;
+class ULeibnizGetPI;
+class UTaylorSeriesApproximationofSin;
+
+UCLASS()
+class GAMEMATH_API AMathBundle : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMathBundle();
+
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UTaylorSeriesApproximationofSin* ApproximationofSin;
+	ULeibnizGetPI* LeibnizGetPI;
+	UMontecarloPI* MontecarloPI;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> dotActorClass;
+
+	void CreatedotActor();
+};
